b27b88ae66cc5135c57a664839043166287142b6
mogaya <mogayainnocent@gmail.com>
2024-09-12 09:06:24 +0300

    asset_identy now returning ast_id

--- backend/add_asset/asset_identity.php
+++ backend/add_asset/asset_identity.php
@@ -7,22 +7,24 @@ $ast_tag = mysqli_real_escape_string($con, $_POST["ast_tag"]);
 $ast_serial = mysqli_real_escape_string($con, $_POST["ast_serial"]);
 $ast_category = mysqli_real_escape_string($con, $_POST["ast_category"]);
 
-
-
-// Flag for success
-$flag['success'] = 0;
+// Initialize the response flag
+$response = [
+    'success' => 0,
+    'ast_id' => null,  // Include ast_id in the response
+];
 
 // Proper query with 'INSERT INTO'
-$query = "INSERT INTO assets (ast_id, ast_name, ast_tag, ast_serial, ast_category)
-          VALUES ('', '$ast_name', '$ast_tag', '$ast_serial', '$ast_category')";
+$query = "INSERT INTO assets (ast_name, ast_tag, ast_serial, ast_category)
+          VALUES ('$ast_name', '$ast_tag', '$ast_serial', '$ast_category')";
 
 // Execute the query
 if (mysqli_query($con, $query)) {
-    $flag['success'] = 1;
+    $response['success'] = 1;
+    $response['ast_id'] = mysqli_insert_id($con);  // Get the last inserted ID
 }
 
 // Return the result as JSON
-echo json_encode($flag);
+echo json_encode($response);
 
 // Close the database connection
 mysqli_close($con);
