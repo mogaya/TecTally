0abee652cd76cad0bb0260fb154fca2ce0e84c9b
mogaya <mogayainnocent@gmail.com>
2024-09-28 07:09:32 +0300

    mobile - user_id link successful

--- backend/asset_categories/mobiles.php
+++ backend/asset_categories/mobiles.php
@@ -1,13 +1,15 @@
 <?php
 require '../connect.php';
 
+$user_id = mysqli_real_escape_string($con, $_GET["user_id"]);
+
 // Initialize response array
 $response = [
     'success' => 0,
     'mobiles' => []
 ];
 
-$query = "SELECT * FROM assets WHERE ast_category = 'Mobile'";
+$query = "SELECT * FROM assets WHERE ast_category = 'Mobile' AND user_id = $user_id ";
 $result = mysqli_query($con, $query);
 
 if ($result) {
